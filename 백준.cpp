9bb3d4bf516b2d9851c43af9d1a2df4af9ca6f12
leehwoo <20171248@game.hoseo.edu>
2021-01-24 20:24:55 +0900

    baekjoon_13015
    별찍기 23

--- 백준.cpp
+++ 백준.cpp
@@ -1,101 +1,49 @@
 #include <iostream>
-#include <cstdlib>
-#include <cstring>
+#define _CRT_SECURE_NO_WARNINGS
 using namespace std;
 
-void star(char** arr,int sizeX, int x, int y, int curveCnt,int curveDir)
-{
-	arr[y][x] = '*';
 
-	for (int i = 0; i < curveCnt; i++)
-	{		
-		if (curveDir == 0)
-		{
-			y--;
-		}		
-		else if (curveDir == 1)
-		{
-			x++;
-		}
-		else if (curveDir == 2)
-		{
-			y++;
-		}
-		else if (curveDir == 3)
-		{
-			x--;
-		}
-		if (y == 0 && x == sizeX) return;
-			arr[y][x] = '*';
-	}
-	
-	if (curveDir == 1)
-		curveCnt += 2;
-	if (curveDir == 3)
-	{		
-		curveCnt += 2; curveDir = -1;
-	}
-		
-	star(arr, sizeX, x, y, curveCnt, curveDir+1);
+
+void draw1(int t, int i, int a, int ss) {
+
+    for (t = 0; t < i; t++) {
+        printf(" ");
+    }
+    printf("*");
+    for (t = 0; t < a - 2; t++) {
+        printf(" ");
+    }
+    printf("*");
+    for (t = ss; t > 0; t--) {
+        printf(" ");
+    }
+    if (i != a - 1)printf("*");
+    for (t = 0; t < a - 2; t++) {
+        printf(" ");
+    }
+    printf("*");
+    printf("\n");
 }
-int main()
-{
-	int N = 0;
-	char** arr = 0;
 
-	cin >> N;
-
-	int sizeY = 0, sizeX = 0;
-
-	if (arr == NULL)
-	{
-		if (N == 1)
-		{
-			sizeY = 1; sizeX = 1;
-		}
-		else 
-		{
-			sizeY = (4 * N) - 1; sizeX = (4 * N) - 3;
-		} 
-
-		arr=(char**)new char* [sizeY];
-
-		for (int i = 0; i < sizeY; i++)
-		{
-			if (i == 1)sizeX = 2;
-			else if (i == sizeY - 1)sizeX = (4 * N) - 3;
-			else sizeX = (4 * N) - 2;
-			arr[i] = (char*) new int[sizeX];
-			memset(arr[i], ' ', sizeof(char) * sizeX);
-		}
-	}
-
-	int startX = sizeX / 2; int startY = sizeY / 2 + 1;
-	int curveCnt = 2; int curveDir = 0;
-
-	if(N==1)
-		arr[0][0] = '*';
-	else
-		star(arr, sizeX, startX, startY, curveCnt, curveDir);
-	
-	for (int i = 0; i < sizeY; i++)
-	{
-		for (int j = 0; j < sizeX; j++)
-		{
-			if (i == 1)sizeX = 2;
-			else if (i == sizeY - 1)sizeX = (4 * N) - 3;
-			else sizeX = (4 * N) - 2;
-			cout << arr[i][j];
-		}
-		cout << endl;
-	}
-	if (arr != NULL)
-	{
-		for (int i = 0; i < sizeY; i++)
-		{
-			delete[] arr[i];
-		}
-		delete[] arr;
-	}
-	return 0;
+void draw2(int a, int ss) {
+    for (int i = 1; i <= a; i++)printf("*");
+    for (int i = 1; i <= ss; i++)printf(" ");
+    for (int i = 1; i <= a; i++)printf("*");
+}
+int main() {
+    int t = 0;
+    int a;
+    int point = 0;
+    int arr[1000000] = { 0, };
+    scanf("%d", &a);
+    for (int i = 2; i < a; i++) {
+        point = 1 + 2 * (i - 2);
+        arr[i - 2] = point;
+    }
+    //for(int i=1;i<=a*2-1;i++)draw2(i,a);
+    draw2(a, 1 + 2 * (a - 2));
+    printf("\n");
+    for (int i = 1; i < a; i++)draw1(t, i, a, 2 * (a - 1 - i) - 1);
+    for (int i = a - 2; i > 0; i--)draw1(t, i, a, 2 * (a - 1 - i) - 1);
+    draw2(a, 1 + 2 * (a - 2));
 }
