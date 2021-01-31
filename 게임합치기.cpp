795f254477489fd5b3a920ff57ea6e4ffbb3173d
leehwoo <20171248@game.hoseo.edu>
2021-01-31 20:14:19 +0900

    오목 오델로 합치기 완성

--- 게임합치기.cpp
+++ 게임합치기.cpp
@@ -1,42 +1,51 @@
 #include <iostream>
-
 #define SIZE_OMOK_BOARD 16
 #define SIZE_OTHELLO_BOARD 8
-
-
 using namespace std;
 
-enum GameType
-{
-	OMOK = 0,
-	OTHELLO = 9
-};
 struct Logic {
 private:
 	int** dat = 0;
+	int type = 0;
 	int size = 0;
 	int turn = 2;
+	int othelloCnt = 0;
+	int omokCnt = 0;
 	bool isStone = false;
+
+	enum dir
+	{
+		RIGHT = 0,
+		LEFT,
+		UP,
+		DOWN,
+		DR,
+		UL,
+		DL,
+		UR		
+	};
 public:
 	
 	void MakeMemory(const int);				//메모리 할당
-	void DeleteMemory(int);					//매모리 해제
+	void DeleteMemory();					//매모리 해제
 
 	int** DatGetter() { return dat; };		//dat 반환
-	int SizeGetter() { return size; };
+	int SizeGetter()  { return size; };		
+	void ResetOmokCnt() { omokCnt = 0; };
 	int SelectGameType();
 	bool CheckCanStone(char*);
 	void AlphaPosition(char*);
 
 	//←→ 방향체크
-	void CheckHorizantal(int, int, int, int, int);
+	void CheckHorizantal(int, int, int, int);
 	//↑↓ 방향체크
-	void CheckVertical(int, int, int, int, int);
+	void CheckVertical(int, int, int, int);
 	//↘↖ 방향 체크
-	void CheckRightDown(int, int, int, int, int);
+	void CheckRightDown(int, int, int, int);
 	//↗↙ 방향체크
-	void CheckRightUp(int, int, int, int, int);
+	void CheckRightUp(int, int, int, int);
 
+	bool WinCheck();
 };
 
 struct Render {
@@ -53,11 +62,17 @@ private:
 		"└─","─┴─","─┘",
 	};
 	string stone[2] = { "○", "●" };
+
+	enum GameType
+	{
+		OMOK = 0,
+		OTHELLO = 9
+	};
 public:
 	
 	void GetDatSize(int** p, int s) { dat = p; size = s; };		//Logic의 dat 받아오기
 
-	void DrawBoard();						//보드 그리기
+	void DrawBoard();											//보드 그리기
 	void DrawHorz(int, int, int, int, int, int);
 	string GetStone(int, int);
 };
@@ -73,7 +88,7 @@ void Logic::MakeMemory(const int type)
 			dat[i] = (int*)new int[size];
 			memset(dat[i], 0, sizeof(int) * size);
 		}
-		if (size == SIZE_OTHELLO_BOARD)
+		if (type == 2)//오델로
 		{
 			//초기 바둑알
 			dat[3][3] = 2, dat[4][4] = 2;
@@ -81,7 +96,7 @@ void Logic::MakeMemory(const int type)
 		}	
 	}
 }
-void Logic::DeleteMemory(int type)
+void Logic::DeleteMemory()
 {
 	if (dat != NULL)
 	{
@@ -92,10 +107,8 @@ void Logic::DeleteMemory(int type)
 		delete[] dat;
 	}
 }
-
 int Logic::SelectGameType()
-{
-	int type = 0;
+{	
 	cout << "1. 오목 2. 오델로"<<endl;
 	cin >> type;
 	if (type == 1)size = SIZE_OMOK_BOARD;				//오목
@@ -109,17 +122,20 @@ bool Logic::CheckCanStone(char* pos)
 	isStone = false;
 	if (dat[s1][s2] != 0) return false;
 	dat[s1][s2] = turn;
-	cout << dat[s1][s2] << " " << turn << endl;
-	CheckHorizantal(s1, s2, turn, 0, 0);
-	CheckVertical(s1, s2, turn, 0, 0);
-	CheckRightDown(s1, s2, turn, 0, 0);
-	CheckRightUp(s1, s2, turn, 0, 0);
-	if (!isStone)
+
+	CheckHorizantal( s1, s2, turn, RIGHT);	othelloCnt = 0;
+	CheckHorizantal(s1, s2, turn, LEFT);	othelloCnt = 0;
+	CheckVertical(s1, s2, turn, DOWN);		othelloCnt = 0;	
+	CheckVertical(s1, s2, turn, UP);		othelloCnt = 0;	
+	CheckRightDown(s1, s2, turn, DR);		othelloCnt = 0;
+	CheckRightDown(s1, s2, turn, UL);		othelloCnt = 0;
+	CheckRightUp(s1, s2, turn, DL);			othelloCnt = 0;
+	CheckRightUp(s1, s2, turn, UR);			othelloCnt = 0;
+	if (type == 2 && !isStone)
 	{
 		dat[s1][s2] = 0;
 		return false;
 	}
-	
 	if (turn == 1)turn = 2;
 	else if (turn == 2)turn = 1;
 	return false;
@@ -134,176 +150,198 @@ void Logic::AlphaPosition(char* pos)
 			pos[i] = pos[i] - 'a' + 10;
 	}
 }
-void Logic::CheckHorizantal(int y, int x, int turn, int cnt, int dir)
+void Logic::CheckHorizantal(int y, int x, int turn, int dir)
 {
-	//→ = (dir=1), ← = (dir=2)
-	if (dir == 0)
-	{
-		if (x == 0)
-			dir = 1;
-		else if (dat[y][x - 1] == 0)
-			dir = 1;
-		else if (x == size - 1)
-			dir = 2;
-		else if (dat[y][x + 1] == 0)
-			dir = 2;
-	}
-	if (dir == 1)
-		if (x == size)return;
-	if (dir == 2)
-		if (x == -1)return;
 	if (dat[y][x] == 0) return;
-	if (dat[y][x] != turn && dat[y][x] != 0)
-		cnt++;
-
-	if (dat[y][x] == turn && cnt != 0)
+	
+	if (type == 1)
 	{
-		cout << "check1" << endl;
-		if (dir == 1)
+		if (dir == RIGHT)
 		{
-			for (int i = 0; i <= cnt; i++)
-				dat[y][x - i] = turn;
+			if (dat[y][x] != dat[y][x + 1])return;
+			else omokCnt++;
 		}
-		if (dir == 2)
+		if (dir == LEFT)
 		{
-			for (int i = 0; i <= cnt; i++)
-				dat[y][x + i] = turn;
+			if (dat[y][x] != dat[y][x - 1])return;
+			else omokCnt++;
 		}
-		isStone = true;
 	}
 
-	if (dir == 1)
-		CheckHorizantal(y, x + 1, turn, cnt, dir);
-	if (dir == 2)
-		CheckHorizantal(y, x - 1, turn, cnt, dir);
+	if (type == 2)
+	{
+		if (dat[y][x] != turn && dat[y][x] != 0)
+			othelloCnt++;
+		if (dat[y][x] == turn && othelloCnt != 0)
+		{
+			cout << "check1" << endl;
+			if (dir == RIGHT)
+			{
+				for (int i = 0; i <= othelloCnt; i++)
+					dat[y][x - i] = turn;
+			}
+			if (dir == LEFT)
+			{
+				for (int i = 0; i <= othelloCnt; i++)
+					dat[y][x + i] = turn;
+			}
+			isStone = true;
+		}
+	}
+	
+
+	if (dir == RIGHT)
+		CheckHorizantal(y, x + 1, turn, dir);
+	if (dir == LEFT)
+		CheckHorizantal(y, x - 1, turn, dir);
 }
-void Logic::CheckVertical(int y, int x, int turn, int cnt, int dir)
+void Logic::CheckVertical(int y, int x, int turn, int dir)
 {
-	//↑ = (dir=1), ↓ = (dir=2)
-	if (dir == 0)
-	{
-		if (y == size - 1)
-			dir = 1;
-		else if (dat[y + 1][x] == 0)
-			dir = 1;
-		else if (y == 0)
-			dir = 2;
-		else if (dat[y - 1][x] == 0)
-			dir = 2;
-	}
-	if (dir == 1)
-		if (y == -1)return;
-	if (dir == 2)
-		if (y == size)return;
 	if (dat[y][x] == 0) return;
-	if (dat[y][x] != turn && dat[y][x] != 0)
-		cnt++;
-
-	if (dat[y][x] == turn && cnt != 0)
+	
+	if (type == 1)
 	{
-		cout << "check2" << endl;
-		if (dir == 1)
+		if (dir == UP)
 		{
-			for (int i = 0; i <= cnt; i++)
-				dat[y + i][x] = turn;
+			if (dat[y][x] != dat[y - 1][x])return;
+			else omokCnt++;
 		}
-		if (dir == 2)
+		if (dir == DOWN)
 		{
-			for (int i = 0; i <= cnt; i++)
-				dat[y - i][x] = turn;
+			if (dat[y][x] != dat[y + 1][x])return;
+			else omokCnt++;
 		}
-		isStone = true;
 	}
-	if (dir == 1)
-		CheckVertical(y - 1, x, turn, cnt, dir);
-	if (dir == 2)
-		CheckVertical(y + 1, x, turn, cnt, dir);
+	if (type == 2)
+	{
+		if (dat[y][x] != turn && dat[y][x] != 0)
+			othelloCnt++;
+		if (dat[y][x] == turn && othelloCnt != 0)
+		{
+			cout << othelloCnt << endl;
+			cout << "check2" << endl;
+			if (dir == UP)
+			{
+				for (int i = 0; i <= othelloCnt; i++)
+					dat[y + i][x] = turn;
+			}
+			if (dir == DOWN)
+			{
+				for (int i = 0; i <= othelloCnt; i++)
+					dat[y - i][x] = turn;
+			}
+			isStone = true;
+		}
+	}
+	
+	if (dir == UP)
+		CheckVertical(y - 1, x, turn, dir);
+	if (dir == DOWN)
+		CheckVertical(y + 1, x, turn, dir);
 }
-void Logic::CheckRightDown(int y, int x, int turn, int cnt, int dir)
+void Logic::CheckRightDown(int y, int x, int turn, int dir)
 {
-	//↘ = (dir=1), ↖ = (dir=2)
-	if (dir == 0)
-	{
-		if (x == 0 || y == 0)
-			dir = 1;
-		else if (dat[y - 1][x - 1] == 0)
-			dir = 1;
-		else if (x == size - 1 || y == size - 1)
-			dir = 2;
-		else if (dat[y + 1][x + 1] == 0)
-			dir = 2;
-	}
-	if (dir == 1)
-		if (y == size || x == size)return;
-	if (dir == 2)
-		if (y == -1 || x == -1)return;
-
+	
 	if (dat[y][x] == 0) return;
-	if (dat[y][x] != turn && dat[y][x] != 0)
-		cnt++;
 
-	if (dat[y][x] == turn && cnt != 0)
+	if (type == 1)
 	{
-		cout << "check3" << endl;
-		if (dir == 1)
+		if (dir == DR)
 		{
-			for (int i = 0; i <= cnt; i++)
-				dat[y - i][x - i] = turn;
+			if (dat[y][x] != dat[y + 1][x + 1])return;
+			else omokCnt++;
 		}
-		if (dir == 2)
+		if (dir == UL)
 		{
-			for (int i = 0; i <= cnt; i++)
-				dat[y + i][x + i] = turn;
+			if (dat[y][x] != dat[y - 1][x - 1])return;
+			else omokCnt++;
 		}
-		//isStone = true;
 	}
-	if (dir == 1)
-		CheckRightDown(y + 1, x + 1, turn, cnt, dir);
-	if (dir == 2)
-		CheckRightDown(y - 1, x - 1, turn, cnt, dir);
+	if (type == 2)
+	{
+		if (dat[y][x] != turn && dat[y][x] != 0)
+			othelloCnt++;
+		if (dat[y][x] == turn && othelloCnt != 0)
+		{
+			cout << "check3" << endl;
+			if (dir == DR)
+			{
+				for (int i = 0; i <= othelloCnt; i++)
+					dat[y - i][x - i] = turn;
+			}
+			if (dir == UL)
+			{
+				for (int i = 0; i <= othelloCnt; i++)
+					dat[y + i][x + i] = turn;
+			}
+			isStone = true;
+		}
+	}
+	
+	if (dir == DR)
+		CheckRightDown(y + 1, x + 1, turn, dir);
+	if (dir == UL)
+		CheckRightDown(y - 1, x - 1, turn, dir);
 }
-void Logic::CheckRightUp(int y, int x, int turn, int cnt, int dir)
+void Logic::CheckRightUp(int y, int x, int turn, int dir)
 {
-	//↙ = (dir=1), ↗ = (dir=2)
-	if (dir == 0)
-	{
-		if (x == size - 1 || y == 0)
-			dir = 1;
-		else if (dat[y - 1][x + 1] == 0)
-			dir = 1;
-		else if (x == 0 || y == size - 1)
-			dir = 2;
-		else if (dat[y + 1][x - 1] == 0)
-			dir = 2;
-	}
-	if (dir == 1)
-		if (y == size || x == -1)return;
-	if (dir == 2)
-		if (y == -1 || x == size)return;
-
+	
 	if (dat[y][x] == 0) return;
-	if (dat[y][x] != turn && dat[y][x] != 0)
-		cnt++;
-
-	if (dat[y][x] == turn && cnt != 0)
+	
+	if (type == 1)
 	{
-		cout << "check4" << endl;
-		if (dir == 1)
+		if (dir == DL)
 		{
-			for (int i = 0; i <= cnt; i++)
-				dat[y - i][x + i] = turn;
+			if (dat[y][x] != dat[y + 1][x - 1])return;
+			else omokCnt++;
 		}
-		if (dir == 2)
+		if (dir == UR)
 		{
-			for (int i = 0; i <= cnt; i++)
-				dat[y + i][x - i] = turn;
+			if (dat[y][x] != dat[y - 1][x + 1])return;
+			else omokCnt++;
 		}
-		isStone = true;
 	}
-	if (dir == 1)
-		CheckRightUp(y + 1, x - 1, turn, cnt, dir);
-	if (dir == 2)
-		CheckRightUp(y - 1, x + 1, turn, cnt, dir);
+	if (type == 2)
+	{
+		if (dat[y][x] != turn && dat[y][x] != 0)
+			othelloCnt++;
+		if (dat[y][x] == turn && othelloCnt != 0)
+		{
+			cout << "check4" << endl;
+			if (dir == DL)
+			{
+				for (int i = 0; i <= othelloCnt; i++)
+					dat[y - i][x + i] = turn;
+			}
+			if (dir == UR)
+			{
+				for (int i = 0; i <= othelloCnt; i++)
+					dat[y + i][x - i] = turn;
+			}
+			isStone = true;
+		}
+	}
+	
+	if (dir == DL)
+		CheckRightUp(y + 1, x - 1, turn, dir);
+	if (dir == UR)
+		CheckRightUp(y - 1, x + 1, turn, dir);
+}
+bool Logic::WinCheck()
+{
+	cout << omokCnt << endl;
+	//오목
+	if (type == 1)
+	{
+		if (omokCnt >= 4)
+		{
+			if (turn == 2)cout << "흑돌의 승리" << endl;
+			else if (turn == 1)cout << "백돌의 승리" << endl;
+			return true;
+		}
+	}
+	ResetOmokCnt();
+	return false;
 }
 void Render::DrawBoard()
 {
@@ -379,28 +417,22 @@ string Render::GetStone(int y, int x)
 int main()
 {
 	int type = 0;
-	char pos[2] = { 0, };
+	char pos[3] = { 0, };
 	Logic logic;
 	Render render;
 
-	
 	type = logic.SelectGameType();
 	logic.MakeMemory(type);
 	render.GetDatSize(logic.DatGetter(),logic.SizeGetter());
-	
-	/*for (int i = 0; i < 8; i++)
-	{
-		for (int j = 0; j < 8; j++)
-			cout << logic.dat[i][j];
-		cout << endl;
-	}*/
+
 	while (1)
 	{
+		system("cls");
 		render.DrawBoard();
 		cout << "Input Position: ";
 		cin >> pos;
 		logic.CheckCanStone(pos);
-		
+		if (logic.WinCheck()) break;		
 	}
-	logic.DeleteMemory(type);
+	logic.DeleteMemory();
 }
