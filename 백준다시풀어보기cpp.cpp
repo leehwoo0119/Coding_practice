f855ab114a4d68925babe2830fafaf03e15bbee5
leehwoo <20171248@game.hoseo.edu>
2021-02-06 10:55:18 +0900

    백준 8958

--- 백준다시풀어보기cpp.cpp
+++ 백준다시풀어보기cpp.cpp
@@ -2,15 +2,31 @@
 #include<string.h>
 
 int main() {
-    char s;
+    int N = 0;
+    char s[81] = { 0, };
+    int cnt = 0;
+    int score = 0;
 
+    scanf("%d", &N);
 
-    while (scanf("%c", &s) != EOF)
+    for (int j = 0; j < N; j++)
     {
-        // getchar();
-
-        // if(strlen(s)==1)break;
-        printf("%c", s);
+        score = 0; cnt = 0;
+        scanf("%s", s);
+        for (int i = 0; i < strlen(s); i++)
+        {
+            if (s[i] == 'O')
+            {
+                cnt++;
+            }
+            if (s[i] == 'X')
+            {
+                cnt = 0;
+            }
+            score += cnt;
+        }
+        printf("%d\n", score);
     }
+
 
 }
