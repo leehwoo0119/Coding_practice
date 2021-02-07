6d89fc92525dab2b3b408df8b638514aa23f75e8
leehwoo <20171248@game.hoseo.edu>
2021-02-07 21:53:46 +0900

    백준 11720

--- 백준다시풀어보기cpp.cpp
+++ 백준다시풀어보기cpp.cpp
@@ -1,32 +1,20 @@
 #include<stdio.h>
 #include<string.h>
 
-int main() {
+int main()
+{
     int N = 0;
-    char s[81] = { 0, };
-    int cnt = 0;
+    char s = 0;
     int score = 0;
-
     scanf("%d", &N);
 
-    for (int j = 0; j < N; j++)
+    for (int i = 0; i <= N; i++)
     {
-        score = 0; cnt = 0;
-        scanf("%s", s);
-        for (int i = 0; i < strlen(s); i++)
-        {
-            if (s[i] == 'O')
-            {
-                cnt++;
-            }
-            if (s[i] == 'X')
-            {
-                cnt = 0;
-            }
-            score += cnt;
-        }
-        printf("%d\n", score);
+        scanf("%c", &s);
+        if (s >= '0' && s <= '9')
+            score += (s - '0');
+        // printf("%c",s);
+
     }
-
-
+    printf("%d ", score);
 }
