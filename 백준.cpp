c522f23c510c9f5d467483d1720d5977724bff2b
leehwoo <20171248@game.hoseo.edu>
2021-02-23 23:15:24 +0900

    백준 15552 STL연습

--- 백준.cpp
+++ 백준.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+
 using namespace std;
 
 int main()
 {
-	//ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	int N = 0;	
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int N = 0;
+	int s1 = 0, s2 = 0;
+	pair<int, int>p1;
 	cin >> N;
-	string arr;
-	vector<pair<int, string>>p1;
 
 	for (int i = 0; i < N; i++)
 	{
-		cin >> arr;
-		p1.push_back({ arr.size(),arr });
+		cin >> s1;
+		cin >> s2;
+		p1 = make_pair(s1, s2);
+		cout << p1.first + p1.second << '\n';
 	}
-	sort(p1.begin(), p1.end());
-	auto it = unique(p1.begin(), p1.end());
-	p1.resize(it - p1.begin());
-	cout<<"----------------------------------"<<endl;
-	cout << p1.size();
-	for (auto x : p1)
-	{
-		cout << x.second << endl;
-	}
+
 	return 0;
 }
