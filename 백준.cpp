ca3cbab137ef21477f4c356ffd1c1bed0994139a
leehwoo <20171248@game.hoseo.edu>
2021-02-24 03:53:16 +0900

    백준 2346 STL연습

--- 백준.cpp
+++ 백준.cpp
@@ -1,21 +1,46 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin.tie(0);
+	ios::sync_with_stdio(0);
 	int N = 0;
-	int s1 = 0, s2 = 0;
-	pair<int, int>p1;
+	int s = 0;
+	vector<pair<int, int>> v;
 	cin >> N;
-
 	for (int i = 0; i < N; i++)
 	{
-		cin >> s1;
-		cin >> s2;
-		p1 = make_pair(s1, s2);
-		cout << p1.first + p1.second << '\n';
+		cin >> s;
+		v.push_back({ i + 1, s });
+		
+	}
+	int move = 0;
+	while (1)
+	{
+		
+		cout << v.front().first << " ";
+		move = v.front().second;
+		v.erase(v.begin());
+		if (v.empty())break;
+		if (move > 0)
+		{
+			for (int i = 0; i < move - 1; i++)
+			{
+				v.push_back(v.front());
+				v.erase(v.begin());
+			}
+		}
+		else
+		{
+			for (int i = 0; i < -move; i++)
+			{
+				v.insert(v.begin(),v.back());
+				v.pop_back();
+			}
+		}
 	}
 
 	return 0;
