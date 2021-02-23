8d78258fe1c6ae3f136bd1d89d142801b4a63eda
leehwoo <20171248@game.hoseo.edu>
2021-02-24 07:58:12 +0900

    백준 5397 STL연습

--- 백준.cpp
+++ 백준.cpp
@@ -2,47 +2,46 @@
 #include <list>
 
 using namespace std;
+char s[1000001];
 int main()
 {
 	cin.tie(0);
 	ios::sync_with_stdio(0);
 	int N = 0;
-	
-	string str;
 
-	cin >> str;
+
 	cin >> N;
-	list<char> l(str.begin(), str.end());
-	auto iter = l.end();
 	for (int i = 0; i < N; i++)
 	{
-		char s;
-		
-		cin >> s;		
-			switch (s)
+		cin >> s;
+		list<char> l;
+		auto iter = l.begin();
+		for (int j = 0; s[j] != 0; j++)
+		{
+			switch (s[j])
 			{
-			case 'L':
+			case '<':
 				if (iter != l.begin()) iter--;
 				break;
-			case 'D':
+			case '>':
 				if (iter != l.end()) iter++;
 				break;
-			case 'B':
+			case '-':
 				if (iter != l.begin()) iter = l.erase(--iter);
 				break;
 			default:
-				char x;
-				cin >> x;
-				iter = l.insert(iter, x);
+				iter = l.insert(iter, s[j]);
 				iter++;
-				break;			
+				break;
 			}
-	
+		}
+		for (auto x = l.begin(); x != l.end(); x++)
+		{
+			cout << *x;
+		}
+		cout << '\n';
 	}
-	for (auto& x : l)
-	{
-		cout << x;
-	}
-	
+
+
 	return 0;
 }
