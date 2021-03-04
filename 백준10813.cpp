ce61b815e5c383e3de2af098b2045d6e69a25114
leehwoo <20171248@game.hoseo.edu>
2021-03-04 18:06:10 +0900

    백준 10813 stl

---
+++ 백준10813.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	
+	int N = 0, M = 0;
+	vector<int>v(N);
+
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++)
+		v.push_back(i);
+
+	int v1 = 0, v2 = 0;
+	auto it = v.begin();
+	for (int i = 0; i < M; i++)
+	{
+		cin >> v1 >> v2 ;
+		//
+		swap(*(it + v1 - 1), *(it + v2 - 1));
+	}
+
+
+	for (int x : v)
+		cout << x << " ";
+	return 0;
+}
