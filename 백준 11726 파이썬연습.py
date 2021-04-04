00ef967e6e913464f5a3342bd40fd2406a95857c
leehwoo <20171248@game.hoseo.edu>
2021-04-05 03:06:12 +0900

    백준 11726 DP
    피보나치수열

---
+++ 백준 11726 파이썬연습.py
@@ -0,0 +1,20 @@
+#from array import *
+#import sys
+#arr = list(map(int,input().split()))
+#arr = [0 for _ in range(7)]
+
+def cal(n):
+   if n<1:return n
+   fir=0
+   sec=1
+
+   for i in range(0,n):
+       sum=fir+sec
+       fir=sec
+       sec=sum
+   return sum
+
+n=input()
+n=int(n)
+#n의 갯수를 1,2로 나누는 경우의수이다. 족ㅁ 전개해보니 피보나치 수열이였다
+print(int(cal(n)%10007))
