8963b27ee8ec54c6edb7988cec78f8b25807fefc
leehwoo <20171248@game.hoseo.edu>
2021-05-16 20:34:02 +0900

    Create 백준 2775 씨샵.cs

---
+++ 백준 2775 씨샵.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            while(n--!=0)
+            {
+                int floor = int.Parse(Console.ReadLine());
+                int ho = int.Parse(Console.ReadLine());
+
+                int[,] arr = new int[floor + 1, ho + 1];
+
+                for (int i = 0; i <= floor; i++) 
+                {
+                    for (int j = 0; j < ho; j++)
+                    {
+                        if (i == 0) arr[i, j] = j + 1;
+                        else
+                        {
+                            for(int t=0;t<=j;t++)
+                            {
+                                arr[i, j] += arr[i - 1, t];
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine(arr[floor, ho - 1]);
+            }
+        }
+    }
+}
