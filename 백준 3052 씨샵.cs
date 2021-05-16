18d05a9e40726c6ba2d919b324c23f60c5fc382d
leehwoo <20171248@game.hoseo.edu>
2021-05-16 17:43:42 +0900

    Create 백준 3052 씨샵.cs

---
+++ 백준 3052 씨샵.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> s = new HashSet<int>();
+            int[] num = new int[10];
+
+            for (int i = 0; i < 10; i++) 
+            {
+                int input= int.Parse(Console.ReadLine());
+                s.Add(input % 42);
+            }
+
+            Console.WriteLine(s.Count());
+        }
+    }
+}
