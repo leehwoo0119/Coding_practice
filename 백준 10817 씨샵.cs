a2569479e892dcfffee4bca8639b9876b10cc947
leehwoo <20171248@game.hoseo.edu>
2021-05-16 17:51:47 +0900

    Create 백준 10817 씨샵.cs

---
+++ 백준 10817 씨샵.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] inputarr = input.Split();
+            int[] num = new int[3];
+            for (int i = 0; i < 3; i++) 
+            {
+                num[i] = int.Parse(inputarr[i]);
+            }
+            Array.Sort(num);
+            Console.WriteLine(num[1]);
+        }
+    }
+}
