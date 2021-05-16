452c1fb88eb12153f8473f0321bfe81cb79884aa
leehwoo <20171248@game.hoseo.edu>
2021-05-16 18:43:28 +0900

    Create 백준 1094 씨샵.cs

---
+++ 백준 1094 씨샵.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int input = int.Parse(Console.ReadLine());
+            
+            int cnt = 0;
+            int stick = 64;
+            while (stick != 0) 
+            {
+                
+                if (stick == input)
+                {
+                    cnt++;
+                    break;
+                }
+                if (input > stick)
+                {
+                    cnt++;
+                    input -= stick;
+                } 
+                 stick /= 2;
+            }
+
+            Console.WriteLine(cnt);
+        }
+    }
+}
